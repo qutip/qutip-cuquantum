41f4ad224765df4674cdd2d346af4c63187c13cc
Eric Giguere <eric.giguere@calculquebec.ca>
2025-08-06 16:41:35 -0400

    Add entrypoint

---
+++ tests/test_family.py
@@ -0,0 +1,12 @@
+""" Tests for qutip_cupy.family. """
+
+import re
+
+from qutip_cupy import family
+
+
+class TestVersion:
+    def test_version(self):
+        pkg, version = family.version()
+        assert pkg == "qutip-cuquantum"
+        assert re.match(r"\d+\.\d+\.\d+.*", version)
